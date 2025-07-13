e1b344651d4fc2f7b68e343d9839aa5f9c1aee66
JiwonJang01 <fx567849@gmail.com>
2025-07-13 14:19:14 +0900

    Initial commit with base gradle and config files

---
+++ src/main/java/com/project/stockpay/StockpayApplication.java
@@ -0,0 +1,13 @@
+package com.project.stockpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/stockpay/StockpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.stockpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
